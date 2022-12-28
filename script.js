9139d3e9904e251620c127b9729f5d3eb992747b
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2022-12-28 00:00:07 -0400

    added functionality for a hamburger menu

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 const modal = document.createElement("div");
+modal.classList.add('modal');
+
+const hamburgerMenu = document.createElement('div');
+hamburgerMenu.classList.add('hamburger-menu')
+
+const closeIcon = document.createElement('img');
+closeIcon.src = 'assets/images/icon-menu-close.svg'
+closeIcon.alt = 'Close Icon for menu';
+closeIcon.classList.add('close-icon');
+
+
 const menuIcon = document.querySelector('#menu-icon');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 
-let toggled = false;
+const menuOptions = document.querySelector('#menu-options')
 
-function toggleMenuIcon(){
-    if(toggled){
-        menuIcon.src = 'assets/images/icon-menu.svg';
-        toggled = false;
-    }
-    else{
-        menuIcon.src = 'assets/images/icon-menu-close.svg';
-        toggled = true;
-    }
-}
+
+
 
 menuIcon.addEventListener('click', ()=>{
-    toggleMenuIcon();
-    modal.classList.add('modal');
-    body.appendChild(modal);
+    openHamburgerMenu();
+})
+
+modal.addEventListener("click", (e)=> {
+    if(e.target !== e.currentTarget) return;
+    closeHamburgerMenu();
 
 })
 
-modal.addEventListener("click", ()=> {
-    toggleMenuIcon();
-    modal.classList.remove('modal');
+closeIcon.addEventListener('click', ()=>{
+    closeHamburgerMenu();
+})
+
+function closeHamburgerMenu() {
+    menuOptions.id = 'menu-options';
+    menuOptions.classList.remove('menu-options-hamburger-menu');
     body.removeChild(modal);
-})
+    modal.removeChild(hamburgerMenu);
+    hamburgerMenu.removeChild(menuOptions);
+}
 
-
+function openHamburgerMenu() {
+    body.appendChild(modal);
+    modal.appendChild(hamburgerMenu);
+    hamburgerMenu.appendChild(closeIcon);
+    menuOptions.id = '';
+    menuOptions.classList.add('menu-options-hamburger-menu');
+    hamburgerMenu.appendChild(menuOptions);
+}
